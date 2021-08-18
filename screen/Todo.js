6575d4b074054f8415858566c2fdda6fb0b76e48
DravenRedgrave <77494310+DravenRedgrave@users.noreply.github.com>
2021-08-18 21:27:19 +0300

    Add files via upload

--- screen/Todo.js
+++ screen/Todo.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import { StyleSheet,SafeAreaView, View,ImageBackground } from 'react-native';
+import { StyleSheet,SafeAreaView, View,ImageBackground,FlatList } from 'react-native';
+import Form from '../components/Form';
 import Header from '../components/Header';
+import TodoItem from '../components/Todoitem';
 
 export default function Todo() {
+
+
+  const [todoItems,setTodoItems] = React.useState([
+    {id:1,text:'React Native 1'},
+    {id:2,text:'React Native 2'},
+    {id:3,text:'React Native 3'},
+  ])
+
+
+  const addTodo = (newItem) =>{
+    setTodoItems([
+      ...todoItems,
+      {...newItem}
+    ])
+  }
+
   return (
     <SafeAreaView style={styles.app}>
       <Header/>
@@ -10,7 +28,18 @@ export default function Todo() {
         source={require('../assets/bg.jpg')}
         style={styles.bg}
        >
-
+            <Form
+              addTodo={addTodo}
+            />
+         <FlatList
+          data={todoItems}
+          keyExtractor={ item => item.id}
+          renderItem={ ({item}) =>(
+            <TodoItem
+              item={item}
+            />
+          )}
+         />
       </ImageBackground>
     </SafeAreaView>
   );
